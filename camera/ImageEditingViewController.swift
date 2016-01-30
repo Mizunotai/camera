985197a99bd7096c9a88c69ffbe44c6ad87ae974
Mizunotai <mizutai731@gmail.com>
2016-01-30 17:46:48 +0900

    turai

--- camera/ImageEditingViewController.swift
+++ camera/ImageEditingViewController.swift
@@ -16,6 +16,7 @@ class ImageEditingViewController: UIViewController {
     var trimView1,trimView2,trimView3,trimView4 : UIImageView!
     var location: CGPoint!
     var currentLocation1,currentLocation2,currentLocation3,currentLocation4 : CGPoint!
+    var tauch1 : Bool?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,15 +25,13 @@ class ImageEditingViewController: UIViewController {
         
         self.trimView1 = UIImageView(frame: CGRectMake(25, 25, 50 , 50))
         trimView1.backgroundColor = UIColor.whiteColor()
-        trimView1.layer.position = CGPoint(x: 25,y: 25)
-        self.imageview.addSubview(trimView1)
+        trimView1.layer.position = CGPoint(x: 25,y: 50)
         trimView1.userInteractionEnabled = true
-        let aSelector = Selector("imageTapped1:")
-        trimView1.addGestureRecognizer(UITapGestureRecognizer(target: self, action:aSelector ))
+        self.view.addSubview(trimView1)
         
         self.trimView2 = UIImageView(frame: CGRectMake(self.imageview.frame.size.width-25, 25, 50, 50))
         trimView2.backgroundColor = UIColor.whiteColor()
-        trimView2.layer.position = CGPoint(x: self.view.frame.size.width-25 ,y: 25)
+        trimView2.layer.position = CGPoint(x: self.view.frame.size.width-25 ,y: 50)
         self.imageview.addSubview(trimView2)
         
         self.trimView3 = UIImageView(frame: CGRectMake(25, self.imageview.frame.size.height-25, 50, 50))
@@ -41,7 +40,7 @@ class ImageEditingViewController: UIViewController {
         self.imageview.addSubview(trimView3)
         
         self.trimView4 = UIImageView(frame:
-                        CGRectMake(self.imageview.frame.size.width-25,self.imageview.frame.size.height-25, 50, 50))
+            CGRectMake(self.imageview.frame.size.width-25,self.imageview.frame.size.height-25, 50, 50))
         trimView4.backgroundColor = UIColor.whiteColor()
         trimView4.layer.position = CGPoint(x: self.imageview.frame.size.width-25,y: self.imageview.frame.size.height-25)
         self.imageview.addSubview(trimView4)
@@ -53,24 +52,20 @@ class ImageEditingViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch: UITouch = touches.first!
+        let location: CGPoint = touch.locationInView(self.view)
+        
+        currentLocation1 = location
+        
+    }
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
         let touch: UITouch = touches.first!
-        location = touch.locationInView(imageview)
-//        NSLog("位置 (%f , %f)", location.x,location.y)
-        currentLocation1 = touch.preciseLocationInView(imageview)
+        location = touch.locationInView(self.view)
+  
         
-//        trimView1.center = CGPointMake(location.x, location.y)
-//      trimView2.center = CGPointMake(location.x, location.y)
-//        if trimView1.center ==  location{
-//        
-//        if trimView1.center.x != currentLocation1.x {
-//            trimView3.center.x = trimView1.center.x
-//            }
-        }
-        func imageTapped1(sender: UITapGestureRecognizer) {
-            print(sender)
+        if trimView1.userInteractionEnabled == false {
             trimView1.center = CGPointMake(location.x, location.y)
             
             if trimView1.center.x != currentLocation1.x {
@@ -78,16 +73,18 @@ class ImageEditingViewController: UIViewController {
             }
             
         }
+        
+        
     }
-    
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
-    }
-    */
-    
+}
+/*
+// MARK: - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+// Get the new view controller using segue.destinationViewController.
+// Pass the selected object to the new view controller.
+}
+*/
+
 
