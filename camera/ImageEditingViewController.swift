60b2ffc58aaa84c89d8c4a5bc17d6391ad4bdd75
Mizunotai <mizutai731@gmail.com>
2016-02-06 16:13:30 +0900

    trimming作ってる

--- camera/ImageEditingViewController.swift
+++ camera/ImageEditingViewController.swift
@@ -12,15 +12,20 @@ import UIKit
 class ImageEditingViewController: UIViewController {
     
     var image :UIImage!
-    @IBOutlet var imageview :UIImageView!
+    @IBOutlet var backImageview :UIImageView!
     var trimView1,trimView2,trimView3,trimView4 : UIImageView!
     var location: CGPoint!
     var currentLocation1,currentLocation2,currentLocation3,currentLocation4 : CGPoint!
+    var startPoint : CGPoint!
+    var isImageInside: Bool?
+    var imageListArray : [UIImageView] = []
+    var imageViewFrame :CGPoint!
+  
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.imageview.image = self.image
+        self.backImageview.image = self.image
         
         self.trimView1 = UIImageView(frame: CGRectMake(25, 25, 50 , 50))
         trimView1.backgroundColor = UIColor.whiteColor()
@@ -28,25 +33,29 @@ class ImageEditingViewController: UIViewController {
         trimView1.userInteractionEnabled = true
         let myTap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "trimView1Moved:")
         self.trimView1.addGestureRecognizer(myTap)
-        self.view.addSubview(trimView1)
+        self.backImageview.addSubview(trimView1)
         
         
-        self.trimView2 = UIImageView(frame: CGRectMake(self.imageview.frame.size.width-25, 25, 50, 50))
+        self.trimView2 = UIImageView(frame: CGRectMake(self.backImageview.frame.size.width-25, 25, 50, 50))
         trimView2.backgroundColor = UIColor.whiteColor()
         trimView2.layer.position = CGPoint(x: self.view.frame.size.width-25 ,y: 50)
-        self.imageview.addSubview(trimView2)
+        self.backImageview.addSubview(trimView2)
         
-        self.trimView3 = UIImageView(frame: CGRectMake(25, self.imageview.frame.size.height-25, 50, 50))
+        self.trimView3 = UIImageView(frame: CGRectMake(25, self.backImageview.frame.size.height-25, 50, 50))
         trimView3.backgroundColor = UIColor.whiteColor()
-        trimView3.layer.position = CGPoint(x: 25,y: self.imageview.frame.size.height-25)
-        self.imageview.addSubview(trimView3)
+        trimView3.layer.position = CGPoint(x: 25,y: self.backImageview.frame.size.height-25)
+        self.backImageview.addSubview(trimView3)
         
         self.trimView4 = UIImageView(frame:
-            CGRectMake(self.imageview.frame.size.width-25,self.imageview.frame.size.height-25, 50, 50))
+            CGRectMake(self.backImageview.frame.size.width-25,self.backImageview.frame.size.height-25, 50, 50))
         trimView4.backgroundColor = UIColor.whiteColor()
-        trimView4.layer.position = CGPoint(x: self.imageview.frame.size.width-25,y: self.imageview.frame.size.height-25)
-        self.imageview.addSubview(trimView4)
+        trimView4.layer.position = CGPoint(x: self.backImageview.frame.size.width-25,y: self.backImageview.frame.size.height-25)
+        self.backImageview.addSubview(trimView4)
         
+        self.imageListArray.append(trimView1)
+        self.imageListArray.append(trimView2)
+        self.imageListArray.append(trimView3)
+        self.imageListArray.append(trimView4)
         // Do any additional setup after loading the view.
     }
     
@@ -57,33 +66,49 @@ class ImageEditingViewController: UIViewController {
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         let touch: UITouch = touches.first!
-        let location: CGPoint = touch.locationInView(self.view)
-        currentLocation1 = location
+        startPoint = touch.locationInView(self.view)
+        //        currentLocation1 = self.trimView1.frame.origin
+        
+        for imageview in self.imageListArray{
+            imageViewFrame = imageview.frame.origin
+            
+            // イメージの範囲
+            let MinX = imageViewFrame.x
+            let MaxX = imageViewFrame.x + self.trimView1!.frame.width
+            let MinY = imageViewFrame.y
+            let MaxY = imageViewFrame.y + self.trimView1!.frame.height
+            
+            // イメージの範囲内をタッチした時のみisImageInsideをtrueにする
+            if (MinX <= startPoint!.x && startPoint!.x <= MaxX) && (MinY <= startPoint!.y && startPoint!.y <= MaxY) {
+                print("Inside of Image")
+                isImageInside = true
+                
+            } else {
+                print("Outside of Image")
+                isImageInside = false
+            }
+        }
     }
     
     override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
         let touch: UITouch = touches.first!
         location = touch.locationInView(self.view)
-        //        print(location)
-        trimView1.center = CGPointMake(location.x, location.y)
-//        trimView2.center = CGPointMake(location.x, location.y)
-        if trimView1.center ==  location{
+        
+        if isImageInside! {
+            // 移動量を計算
+            let deltaX: CGFloat = CGFloat(location.x - startPoint!.x)
+            let deltaY: CGFloat = CGFloat(location.y - startPoint!.y)
             
-            if trimView1.center.x != currentLocation1.x {
-                trimView3.center.x = trimView1.center.x
-            }
+            // イメージを移動
+            self.trimView1.frame.origin.x = currentLocation1!.x + deltaX
+            self.trimView1.frame.origin.y = currentLocation1!.y + deltaY
+        }else {
+            // Do nothing
         }
+        
         
     }
     
-    //    func tapGesture(sender:UITapGestureRecognizer){
-    ////        self.trimView1sMoved()
-    //
-    //    }
-    //    func trimView1Moved (sender: UITapGestureRecognizer,withEvent touches: Set<UITouch>, withEvent event: UIEvent? ) {
-    //
-    //         print("touch")
-    //    }
 }
 /*
 // MARK: - Navigation
