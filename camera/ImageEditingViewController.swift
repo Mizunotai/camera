708decfb057482b2484de4da7ac775a835ec1e18
Mizunotai <mizutai731@gmail.com>
2016-02-06 17:02:03 +0900

    画像ごとに移動できるようにした

--- camera/ImageEditingViewController.swift
+++ camera/ImageEditingViewController.swift
@@ -14,13 +14,15 @@ class ImageEditingViewController: UIViewController {
     var image :UIImage!
     @IBOutlet var backImageview :UIImageView!
     var trimView1,trimView2,trimView3,trimView4 : UIImageView!
+    var moveView: UIImageView!
+    
     var location: CGPoint!
     var currentLocation1,currentLocation2,currentLocation3,currentLocation4 : CGPoint!
     var startPoint : CGPoint!
     var isImageInside: Bool?
     var imageListArray : [UIImageView] = []
     var imageViewFrame :CGPoint!
-  
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,9 +32,6 @@ class ImageEditingViewController: UIViewController {
         self.trimView1 = UIImageView(frame: CGRectMake(25, 25, 50 , 50))
         trimView1.backgroundColor = UIColor.whiteColor()
         trimView1.layer.position = CGPoint(x: 25,y: 50)
-        trimView1.userInteractionEnabled = true
-        let myTap:UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "trimView1Moved:")
-        self.trimView1.addGestureRecognizer(myTap)
         self.backImageview.addSubview(trimView1)
         
         
@@ -67,47 +66,56 @@ class ImageEditingViewController: UIViewController {
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         let touch: UITouch = touches.first!
         startPoint = touch.locationInView(self.view)
-        //        currentLocation1 = self.trimView1.frame.origin
-        
-        for imageview in self.imageListArray{
-            imageViewFrame = imageview.frame.origin
-            
-            // イメージの範囲
-            let MinX = imageViewFrame.x
-            let MaxX = imageViewFrame.x + self.trimView1!.frame.width
-            let MinY = imageViewFrame.y
-            let MaxY = imageViewFrame.y + self.trimView1!.frame.height
-            
-            // イメージの範囲内をタッチした時のみisImageInsideをtrueにする
-            if (MinX <= startPoint!.x && startPoint!.x <= MaxX) && (MinY <= startPoint!.y && startPoint!.y <= MaxY) {
-                print("Inside of Image")
-                isImageInside = true
-                
+               
+        for imageView in self.imageListArray {
+            if self.compareViewPositon(imageView) {
+                self.isImageInside = true
+                break
             } else {
-                print("Outside of Image")
-                isImageInside = false
+                self.isImageInside = false
             }
         }
     }
     
-    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        let touch: UITouch = touches.first!
-        location = touch.locationInView(self.view)
+    
+    func compareViewPositon(imageView: UIImageView) -> Bool {
+        let imageViewFrame = imageView.frame.origin
         
-        if isImageInside! {
-            // 移動量を計算
-            let deltaX: CGFloat = CGFloat(location.x - startPoint!.x)
-            let deltaY: CGFloat = CGFloat(location.y - startPoint!.y)
+        // イメージの範囲
+        let MinX = imageViewFrame.x
+        let MaxX = imageViewFrame.x + imageView.frame.width
+        let MinY = imageViewFrame.y
+        let MaxY = imageViewFrame.y + imageView.frame.height
+        
+        if (MinX <= startPoint!.x && startPoint!.x <= MaxX) && (MinY <= startPoint!.y && startPoint!.y <= MaxY) {
+            print("Inside of Image")
+            self.moveView = imageView
+            return true
+        } else {
+            print("Outside of Image")
+            return false
+        }
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        if self.isImageInside! {
+            let touch: UITouch = touches.first!
+            location = touch.locationInView(self.view)
             
             // イメージを移動
-            self.trimView1.frame.origin.x = currentLocation1!.x + deltaX
-            self.trimView1.frame.origin.y = currentLocation1!.y + deltaY
-        }else {
-            // Do nothing
+            
+            moveView.center = CGPointMake(location.x,location.y)
+        } else {
+            
         }
         
         
+        
     }
+    
+    
+    
     
 }
 /*
